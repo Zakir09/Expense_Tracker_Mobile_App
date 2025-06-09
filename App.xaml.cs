d05feaa428730d39644633c1a759ae2d6dc2f4cc
Zakir Ahmed <zakirahme2002@hotmail.com>
2025-06-09 12:09:51 +0100

    Add base application setup with App.xaml and App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ExpenseTracker.Views;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using Plugin.LocalNotification;
+using Plugin.LocalNotification.EventArgs;
+
+namespace ExpenseTracker
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+			// Local Notification tap event listener
+			LocalNotificationCenter.Current.NotificationActionTapped += OnNotificationActionTapped;
+
+			MainPage = new NavigationPage(new MainPage()); Application.Current.UserAppTheme = AppTheme.Light;
+			this.RequestedThemeChanged += (s, e) => { Application.Current.UserAppTheme = AppTheme.Light; };
+		}
+		private void OnNotificationActionTapped(NotificationActionEventArgs e)
+		{
+			if (e.IsDismissed)
+			{ 
+				return;
+			}
+			if (e.IsTapped)
+			{
+				MainPage = new NavigationPage(new RemindersPage());
+				return;
+			}
+
+		}
+	}
+}
